c0812ac1e5bea12892be5bc742fa322c013edd37
Allana <allanac.avila@gmail.com>
2024-10-10 17:29:36 -0300

    criando centro e entrega

--- src/centro_distribuicao.py
+++ src/centro_distribuicao.py
@@ -1,1 +1,22 @@
 class CentroDistribuicao:
+    def __init__(self, cidade, estado):
+        self.cidade = cidade
+        self.estado = estado
+        self.caminhoes = []
+
+    def adicionarCaminhoes(self, caminhao):
+        self.caminhoes.append(caminhao)
+
+    def desparcharCaminhoes(self, entrega):
+        for caminhao in self.caminhoes:
+            if caminhao.isDisponivel():
+               if caminhao.carregarEntrega(entrega.volume):
+                   caminhao.atualizarLocalizacao(entrega.destino)
+                   caminhao.disponivel = False
+                   return caminhao
+        return None
+
+    def listarCaminhoes(self):
+        return self.caminhoes
+
+
--- src/entrega.py
+++ src/entrega.py
@@ -1,1 +1,12 @@
 class Entrega:
+    def __init__(self, idEntrega, destino, volume, prazo):
+        self.idEntrega = idEntrega
+        self.destino = destino
+        self.volume = volume
+        self.prazo = prazo
+        self.status = "Pendente"
+
+    def atualizarStatus(self, status):
+        self.status = status
+
+
